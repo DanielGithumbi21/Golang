634822e2ab4648951b104eb5d5352ba878b67923
DanielGithumbi21 <danielgithumbi1998@gmail.com>
2023-07-11 14:03:58 +0300

    flow control and looping

--- Basics/Basics.go
+++ Basics/Basics.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"math"
 	"math/cmplx"
+	"time"
 )
 
 // BASIC FUNCTIONAL CALLS
@@ -51,7 +52,55 @@ var i int            //Gives zero for numeric value
 var isLoggedIn bool  // false for boolean values
 var greetings string // "" empty quotes for strings
 
+// CONTROL FLOW STATEMENTS
+// IF
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
+
+func ageMeter(age int) string {
+	if age < 18 {
+		return "under age"
+	} else {
+		return "Right age"
+	}
+}
 func main() {
+	// LOOPS
+	// Only for loop in go
+	sum := 0
+
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println((sum))
+
+	// using FOR as WHILE
+	number := 1
+	for number < 1000 {
+		number += number
+	}
+	fmt.Println(number)
+
+	//SWITCH statements
+	fmt.Println("When's today?")
+	today := time.Now().Weekday()
+
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today")
+	case today + 1:
+		fmt.Println("Tomorrow")
+	case today + 2:
+		fmt.Println("In two days")
+	default:
+		fmt.Println("Too far away")
+	}
+	// Switch without a condition is the same as switch true.
+
 	//Type conversion
 	var n, p int = 3, 4
 	var h float64 = math.Sqrt(float64(n*n + p*p))
@@ -66,6 +115,8 @@ func main() {
 	var i string
 	// Short variable declarations (only used inside functions)
 	length := 11
+	fmt.Println(ageMeter(17))
+	fmt.Println(sqrt(2), sqrt(-4))
 	fmt.Println(z)
 	fmt.Println(split(17))
 	fmt.Println(i, python, java, c)
@@ -75,4 +126,18 @@ func main() {
 	fmt.Printf("Type: %T Value: %v\n", hello, hello)
 	fmt.Printf("Type: %T Value: %v\n", maxInt, maxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	// Deferring a function
+	//A defer statement defers the execution of a function until the surrounding function returns.
+	defer fmt.Println("World")
+	fmt.Println("Hello")
+	//Deferred function calls are pushed onto a stack.
+	//When a function returns, its deferred calls are executed in last-in-first-out order.
+	fmt.Println("counting")
+
+	for i := 0; i < 10; i++ {
+		defer fmt.Println(i)
+	}
+
+	fmt.Println("done")
 }
---
+++ Exercises/Exercise.go
@@ -0,0 +1,31 @@
+// As a way to play with functions and loops, let's implement a square root function:
+//given a number x, we want to find the number z for which z² is most nearly x.
+// Computers typically compute the square root of x using a loop.
+//Starting with some guess z, we can adjust z based on how close z² is to x, producing a better guess:
+// z -= (z*z - x) / (2*z)
+// Repeating this adjustment makes the guess better and better until we reach an answer
+//that is as close to the actual square root as can be.
+
+// Implement this in the func Sqrt provided. A decent starting guess for z is 1, no matter what the input.
+//To begin with, repeat the calculation 10 times and print each z along the way.
+//See how close you get to the answer for various values of x (1, 2, 3, ...) and how quickly the guess improves.
+
+// SOLUTION
+package main
+
+import (
+	"fmt"
+)
+
+func sqrt(x float64) float64 {
+	z := 1.0
+	for i := 0; i < 10; i++ {
+		z -= (z*z - x) / (2 * z)
+		fmt.Println(z)
+	}
+	return z
+}
+
+func main() {
+	fmt.Println(sqrt(2))
+}
